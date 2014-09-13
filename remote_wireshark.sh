da4c3812e03ebe17d1072bdbcac285363ee980c9
Paul McMillan <paul.mcmillan@nebula.com>
2014-09-12 23:33:07 -0700

    add capture script

--- remote_wireshark.sh
+++ remote_wireshark.sh
@@ -1,4 +1,5 @@
 rm -f /tmp/remote_wireshark
 mkfifo /tmp/remote_wireshark
 wireshark -k -i /tmp/remote_wireshark &
-ssh v2 "tcpdump -l -i any -w - 'not tcp port 22' 2>/dev/null" > /tmp/remote_wireshark
+ssh v2 "tcpdump -l -i eth2 -w - 'not tcp port 22' 2>/dev/null" > /tmp/remote_wireshark
+rm -f /tmp/remote_wireshark
